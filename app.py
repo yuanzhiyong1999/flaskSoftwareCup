2f2aa8c4af33b0dc6263b288399ebdeaded10238
yuanzhiyong <yuanzhiyong1999@163.com>
2022-07-03 20:34:01 +0800

    2022.7.3

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import traceback
 import cv2
 import numpy as np
 from PIL import Image
+from cv2 import waitKey
 from flask import Flask, request
 import paddlers as pdrs
 from skimage import img_as_ubyte
+import urllib
 
 import unifiedResponse
-from skimage.io import imread, imsave
+from skimage.io import imread, imsave, show
 
 import utils
 
@@ -21,11 +23,14 @@ te = pdrs.deploy.Predictor('model/inference_model/target_extraction')
 
 @app.route('/target_extraction', methods=['POST'])
 def target_extraction():
-    image = request.files.get('image')
-    if image is not None:
+    image_url = request.form.get('image')
+    if image_url is not None:
+        resp = urllib.request.urlopen(image_url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         utils.judge_path('target_extraction')
         try:
-            result = te.predict(img_file=np.array(Image.open(image)))
+            result = te.predict(img_file=image)
             path = utils.get_path_name('target_extraction') + '-result.png'
             imsave(path, result['label_map'], check_contrast=False)
             url = utils.uploadPic(path)
@@ -50,13 +55,18 @@ cd = pdrs.deploy.Predictor('model/inference_model/change_detection')
 
 @app.route('/change_detection', methods=['POST'])
 def change_detection():
-    before = request.files.get('before')
-    after = request.files.get('after')
-    if before is not None and after is not None:
+    before_url = request.form.get('before')
+    after_url = request.form.get('after')
+    if before_url is not None and after_url is not None:
+        resp1 = urllib.request.urlopen(before_url)
+        resp2 = urllib.request.urlopen(after_url)
+        before = np.asarray(bytearray(resp1.read()), dtype="uint8")
+        after = np.asarray(bytearray(resp2.read()), dtype="uint8")
+        before = cv2.imdecode(before, cv2.IMREAD_COLOR)
+        after = cv2.imdecode(after, cv2.IMREAD_COLOR)
         utils.judge_path('change_detection')
         try:
-            result = cd.predict(img_file=(np.array(Image.open(before)), np.array(Image.open(after))))
-
+            result = cd.predict(img_file=(before, after))
             path = utils.get_path_name('change_detection') + '-result.png'
             imsave(path, result[0]['label_map'], check_contrast=False)
             url = utils.uploadPic(path)
@@ -74,13 +84,35 @@ def change_detection():
 td = pdrs.deploy.Predictor('model/inference_model/target_detection')
 
 
+@app.route('/test', methods=['POST'])
+def test():
+    try:
+        img_src = "http://cup.lijx.cloud/img/gallery/2022/7/2/ad4a5ace-ab67-4a1a-8c00-06ee1ce9feb4"
+        cap = cv2.VideoCapture(img_src)
+        if (cap.isOpened()):
+            ret, img = cap.read()
+            result = td.predict(img_file=np.array(img))
+            # path = utils.get_path_name('target_detection') + '-result.png'
+            # imsave(path, result['label_map'], check_contrast=False)
+            # url = utils.uploadPic(path)
+            # utils.delete_temp_pic(path)
+
+            return unifiedResponse.result(data=result[0])
+    except Exception as e:
+        print(traceback.format_exc())
+        return unifiedResponse.error('图片预测失败')
+
+
 @app.route('/target_detection', methods=['POST'])
 def target_detection():
-    image = request.files.get('image')
-    if image is not None:
+    image_url = request.form.get('image')
+    if image_url is not None:
+        resp = urllib.request.urlopen(image_url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         utils.judge_path('target_detection')
         try:
-            result = td.predict(img_file=np.array(Image.open(image)))
+            result = td.predict(img_file=image)
             # path = utils.get_path_name('target_detection') + '-result.png'
             # imsave(path, result['label_map'], check_contrast=False)
             # url = utils.uploadPic(path)
@@ -100,11 +132,14 @@ tc = pdrs.deploy.Predictor('model/inference_model/terrain_classification')
 
 @app.route('/terrain_classification', methods=['POST'])
 def terrain_classification():
-    image = request.files.get('image')
-    if image is not None:
+    image_url = request.form.get('image')
+    if image_url is not None:
+        resp = urllib.request.urlopen(image_url)
+        image = np.asarray(bytearray(resp.read()), dtype="uint8")
+        image = cv2.imdecode(image, cv2.IMREAD_COLOR)
         utils.judge_path('terrain_classification')
         try:
-            result = tc.predict(img_file=np.array(Image.open(image)))
+            result = tc.predict(img_file=image)
             path = utils.get_path_name('terrain_classification') + '-result.png'
             imsave(path, result['label_map'], check_contrast=False)
             url = utils.uploadPic(path)
--- unifiedResponse.py
+++ unifiedResponse.py
@@ -18,8 +18,8 @@ class HttpCode(object):
     error = 444
 
 
-def result(code=HttpCode.ok, message='', data=None, kwargs=None):
-    json_dict = {'code': code, 'message': message, 'data': data}
+def result(code=HttpCode.ok, msg='', data=None, kwargs=None):
+    json_dict = {'code': code, 'msg': msg, 'data': data}
 
     if kwargs and isinstance(kwargs, dict) and kwargs.keys():
         json_dict.update(kwargs)
@@ -31,21 +31,21 @@ def ok():
     return result()
 
 
-def error(message=''):
-    return result(code=HttpCode.error, message=message)
+def error(msg=''):
+    return result(code=HttpCode.error, msg=msg)
 
 
-def params_error(message='', data=None):
-    return result(code=HttpCode.paramserror, message=message, data=data)
+def params_error(msg='', data=None):
+    return result(code=HttpCode.paramserror, msg=msg, data=data)
 
 
-def unauth(message='', data=None):
-    return result(code=HttpCode.unauth, message=message, data=data)
+def unauth(msg='', data=None):
+    return result(code=HttpCode.unauth, msg=msg, data=data)
 
 
-def method_error(message='', data=None):
-    return result(code=HttpCode.methoderror, message=message, data=data)
+def method_error(msg='', data=None):
+    return result(code=HttpCode.methoderror, msg=msg, data=data)
 
 
-def server_error(message='', data=None):
-    return result(code=HttpCode.servererror, message=message, data=data)
+def server_error(msg='', data=None):
+    return result(code=HttpCode.servererror, msg=msg, data=data)
